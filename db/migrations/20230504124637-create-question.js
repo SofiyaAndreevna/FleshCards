c3bef5f753b5718fb2a0a439e4ede7d59b1b7f52
Pro100Seva <egorovgenius@mail.ru>
2023-05-04 17:19:03 +0300

    dataBase without seeds

---
+++ db/migrations/20230504124637-create-question.js
@@ -0,0 +1,44 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Questions", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      question: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      titleId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Titles",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+      },
+      answer: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      points: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Questions");
+  },
+};
